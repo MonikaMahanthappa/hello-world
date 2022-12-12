624060988cfd12d12a27e14c866be4abd9594a2f
monika <monikamahanthappa@gmail.com>
2022-12-12 12:14:37 +0530

    Add. Fizz for multiple of three

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -3,5 +3,8 @@ package fizzbuzz
 import "strconv"
 
 func FizzBuzz(number int) string {
+	if number%3 == 0 {
+		return "Fizz"
+	}
 	return strconv.Itoa(number)
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -10,9 +10,17 @@ func TestFizzBuzz(t *testing.T) {
 	t.Run("should be 1 when number is 1", func(t *testing.T) {
 		assert.Equal(t, "1", fizzbuzz.FizzBuzz(1))
 	})
-	
+
 	t.Run("should be 4 when number is 4", func(t *testing.T) {
 		assert.Equal(t, "4", fizzbuzz.FizzBuzz(4))
+	})
+
+	t.Run("should be Fizz when number is 3", func(t *testing.T) {
+		assert.Equal(t, "Fizz", fizzbuzz.FizzBuzz(3))
+	})
+
+	t.Run("should be Fizz when number is 6", func(t *testing.T) {
+		assert.Equal(t, "Fizz", fizzbuzz.FizzBuzz(6))
 	})
 
 }
